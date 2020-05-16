2ebcec35f0cb56d9a01e1deb7cc59ac84897e275
Shivam Agrawal <shagra26@in.ibm.com>
2020-05-16 13:18:25 +0530

    Updated enum.

--- src/main/java/com/training/recipe/domain/Recipe.java
+++ src/main/java/com/training/recipe/domain/Recipe.java
@@ -22,6 +22,8 @@ public class Recipe {
     private String source;
     private String url;
     private String directions;
+
+    @Enumerated(value = EnumType.STRING)
     private Difficulty difficulty;
 
     @Lob
