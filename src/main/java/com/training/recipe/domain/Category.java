5d6754b06bd85397db6f3070500f997f98038e4a
Shivam Agrawal <shagra26@in.ibm.com>
2020-05-16 13:38:45 +0530

    Added Category entity

---
+++ src/main/java/com/training/recipe/domain/Category.java
@@ -0,0 +1,46 @@
+package com.training.recipe.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+
+/**
+ * Created by ShivamAgrawal on 16-05-2020
+ * inside the package - com.training.recipe.domain
+ */
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+}
