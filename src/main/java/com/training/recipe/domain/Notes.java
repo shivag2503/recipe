e171d725d59a858168f32fc338a0fa1ec04fb98e
Shivam Agrawal <shagra26@in.ibm.com>
2020-05-16 12:46:11 +0530

    Recipe and Notes entities are added.

---
+++ src/main/java/com/training/recipe/domain/Notes.java
@@ -0,0 +1,46 @@
+package com.training.recipe.domain;
+
+import javax.persistence.*;
+
+/**
+ * Created by Shivam Agrawal on 16-05-2020
+ * inside the package - com.training.recipe.domain
+ */
+
+@Entity
+public class Notes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    private Recipe recipe;
+
+    @Lob
+    private String recipeNotes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
+}
