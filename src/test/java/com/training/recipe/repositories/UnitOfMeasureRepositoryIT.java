805c5d4ca882c614cef63e76bd815c1133cfb1ad
Shivam Agrawal <shagra26@in.ibm.com>
2020-05-26 21:41:43 +0530

    Integration test implemented.

---
+++ src/test/java/com/training/recipe/repositories/UnitOfMeasureRepositoryIT.java
@@ -0,0 +1,33 @@
+package com.training.recipe.repositories;
+
+import com.training.recipe.domain.UnitOfMeasure;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+class UnitOfMeasureRepositoryIT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findByUom() {
+
+        Optional<UnitOfMeasure> unitOfMeasure = unitOfMeasureRepository.findByUom("Teaspoon");
+
+        assertEquals("Teaspoon", unitOfMeasure.get().getUom());
+    }
+}
